39eac9f9d96cd6dac96db8681482ca80ade27501
coderajay94 <official.ajaykumar94@gmail.com>
2024-05-11 23:54:28 +0530

    constants practice

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+//outside main variable declaration
+var JWTToken = "token data"
+
+//constants declaration
+const APIKey = "API key"
+
 func main() {
 	fmt.Println("variables")
+
+	var num1 int = 23
+
+	fmt.Printf("%T  and %v \n", num1, num1)
+
+	var monthly float32 = 1200.12349
+	var salary float64 = 120990.1234567890111213141516171811920
+
+	fmt.Println("monthly and yearly salary", monthly, salary)
+
+	fmt.Println(JWTToken)
+
+	fmt.Println(APIKey)
+
+	//only works inside the function
+	//automaticlly detects the type
+	data := "hello world"
+	fmt.Printf("%v, %T \n", data, data)
+
 }
